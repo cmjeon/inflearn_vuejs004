00299ba3a80adbf7c31a223a2cc02caa491d7a6a
cmj <chmin82@gmail.com>
2020-09-13 22:21:55 +0900

    3-2

--- vue-todo/src/components/TodoHeader.vue
+++ vue-todo/src/components/TodoHeader.vue
@@ -1,15 +1,9 @@
 <template>
-    <div>
-        header
-    </div>
+    <header>
+        <h1>TODO it!</h1>
+    </header>
 </template>
 
-<script>
-export default {
-    
-}
-</script>
-
-<style>
+<style scoped>
 
 </style>
