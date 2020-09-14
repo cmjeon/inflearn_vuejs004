adeb8200d4008262d7fe4cb41591d2b791a618ae
cmj <chmin82@gmail.com>
2020-09-14 21:12:39 +0900

    3-5

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
+  <div class="inputBox shadow">
     <input type="text" v-model="newTodoItem">
-    <button v-on:click="addTodo">add</button>
+    <!-- <button v-on:click="addTodo">add</button> -->
+    <span class="addContainer" v-on:click="addTodo" v-on:keyup.enter="addTodo">
+        <i class="fas fa-plus addBtn"></i>
+    </span>
   </div>
 </template>
 
@@ -17,11 +20,37 @@ export default {
             localStorage.setItem(this.newTodoItem, this.newTodoItem);
             this.newTodoItem = '';
         },
+        clearInput: function() {
+            this.newTodoItem;
+        }
 
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+input:focus {
+    outline: none;
+}
+.inputBox {
+    background: white;
+    height:50px;
+    line-height:50px;
+    border-radius:5px;
+}
+.inputBox input {
+    border-style:none;
+    font-size:0.9rem;
+}
+.addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478FB, #8763FB);
+    display: block;
+    width:3rem;
+    border-radius: 0 5px 5px 0;
+}
+.addBtn {
+    color: white;
+    vertical-align: middle;
+}
 </style>
