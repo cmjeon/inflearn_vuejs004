9b9bb628b771b9a1613aa38252b9c11bc7b2520b
cmj <chmin82@gmail.com>
2020-09-16 15:02:18 +0900

    4-3

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput></TodoInput>
+    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
     <TodoList v-bind:propsdata="todoItems"></TodoList>
     <TodoFooter></TodoFooter>
   </div>
@@ -30,6 +30,13 @@ export default {
           this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));          
         }
       }
+    }
+  },
+  methods: {
+    addOneItem: function(todoItem) {  
+      var obj = { completed: false, item: todoItem};
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      this.todoItems.push(obj);
     }
   },
   components: {
