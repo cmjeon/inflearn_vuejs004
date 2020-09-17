ac5bf0f1b386db1a720159a37666febe5c16c84b
cmj <chmin82@gmail.com>
2020-09-17 15:10:51 +0900

    ES6 for Vue.js - Enhanced Object Literals - 2

--- vue-todo/src/App.vue
+++ vue-todo/src/App.vue
@@ -22,12 +22,12 @@ var my_cmp = {
 };
 
 export default {
-  data: function() {
+  data() {
     return {
       todoItems:[]
     }
   },
-  created: function() {
+  created() {
     if(localStorage.length > 0) {
       for(let i = 0; i < localStorage.length; i++) {
         if(localStorage.key(i) !== 'loglevel:webpack-dev-server') {
@@ -37,21 +37,21 @@ export default {
     }
   },
   methods: {
-    addOneItem: function(todoItem) {  
-      const obj = { completed: false, item: todoItem};
+    addOneItem(todoItem) {  
+      const obj = { completed: false, item: todoItem };
       localStorage.setItem(todoItem, JSON.stringify(obj));
       this.todoItems.push(obj);
     },
-    removeOneItem: function(todoItem, index) {
+    removeOneItem(todoItem, index) {
       localStorage.removeItem(todoItem.item);
       this.todoItems.splice(index, 1);
     },
-    toggleOneItem: function(todoItem, index) {
+    toggleOneItem(todoItem, index) {
       this.todoItems[index].completed = !this.todoItems[index].completed;
       localStorage.removeItem(todoItem.item);
       localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     },
-    clearAllItems: function() {
+    clearAllItems() {
       localStorage.clear();
       this.todoItems=[];
     }
