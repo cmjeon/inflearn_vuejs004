83f300dd67d13a5ee66b215388a049bac653a0a0
cmj <chmin82@gmail.com>
2020-09-21 23:09:32 +0900

    Vuex- 주요 기술 요소 - 6

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -20,11 +20,11 @@ export default {
   methods: {
     removeTodo(todoItem, index) {
       // this.$emit('removeItem', todoItem, index);
-      this.$store.commit('removeOneItem', { todoItem:todoItem, index:index });
+      this.$store.commit('removeOneItem', { todoItem, index });
     },
     toggleComplete(todoItem, index) {
       // this.$emit('toggleItem', todoItem, index);
-      this.$store.commit('toggleOneItem', { todoItem:todoItem, index:index });
+      this.$store.commit('toggleOneItem', { todoItem, index });
     }
   }
 }
